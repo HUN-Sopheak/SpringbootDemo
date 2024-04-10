fbabfb2a03203f6c14227babaada7e9b60ba4e86
Shuan-pc <Sopheakhun030@gmail.com>
2024-04-10 09:04:49 +0700

    update

--- src/main/java/com/api/demo/Controller/TestController.java
+++ src/main/java/com/api/demo/Controller/TestController.java
@@ -3,12 +3,17 @@ package com.api.demo.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
 public class TestController {
     
     @GetMapping("/test")
     public String getTest() {
-        return "<h1>Test</h1>";
+        return "<h1>Hello Spring boot</h1>";
+    }
+
+    @GetMapping("/test/test2")
+    public String getTest2() {
+        return "<h1>Hello Spring boot 2</h1>";
     }
 }
 
